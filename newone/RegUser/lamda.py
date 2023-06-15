f79c7ecedf1875a01d3ad2b49ed1a0da5acfd474
Pratikisback <pratikgopal.cha26@gmail.com>
2023-06-15 15:22:29 +0530

    first commit

---
+++ newone/RegUser/lamda.py
@@ -0,0 +1,11 @@
+fruits = ["apkiwiple", "banana", "cherry", "kiwi", "mankiwigo"]
+# jl = [x for x in fruits if x!="banana"]
+# print(jl)
+#
+# bl = [ x.replace("kiwi", "grapes") for x in fruits]
+
+# print(l)
+# bl = [x for x in range(1,778) if x%7 == 0]
+# print(bl)
+
+
